b158a6396f32d9c17598dc556757af5dc77cd866
Thomas Aubert <aubert.thms@gmail.com>
2023-09-24 21:42:31 +0200

    add header description and fix indentation

--- include/type_map.h
+++ include/type_map.h
@@ -1,3 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * MIT License                                                                     *
+ *                                                                                 *
+ * Copyright (c) 2023 Thomas AUBERT                                                *
+ *                                                                                 *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy    *
+ * of this software and associated documentation files (the "Software"), to deal   *
+ * in the Software without restriction, including without limitation the rights    *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       *
+ * copies of the Software, and to permit persons to whom the Software is           *
+ * furnished to do so, subject to the following conditions:                        *
+ *                                                                                 *
+ * The above copyright notice and this permission notice shall be included in all  *
+ * copies or substantial portions of the Software.                                 *
+ *                                                                                 *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   *
+ * SOFTWARE.                                                                       *
+ *                                                                                 *
+ * github : https://github.com/ThomasAUB/TypeMap/tree/main                         *
+ *                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #pragma once
 
 #include <type_traits>
@@ -6,17 +33,26 @@ namespace typemap {
 
     namespace detail {
 
-        template<auto key, typename ... pairs>
-        struct KeyFinder;
+        template<auto key, typename ... pairs> struct KeyFinder;
 
         template<auto key, typename pair_t>
         struct KeyFinder<key, pair_t> {
-            using type = typename std::conditional<key == pair_t::key, typename pair_t::type, std::false_type>::type;
+            using type = 
+                typename std::conditional<
+                    key == pair_t::key, 
+                    typename pair_t::type, 
+                    std::false_type
+                >::type;
         };
 
         template<auto key, typename pair1_t, typename ... pairs_rest>
         struct KeyFinder<key, pair1_t, pairs_rest...> {
-            using type = typename std::conditional<key == pair1_t::key, typename pair1_t::type, typename KeyFinder<key, pairs_rest...>::type>::type;
+            using type = 
+                typename std::conditional<
+                    key == pair1_t::key, 
+                    typename pair1_t::type, 
+                    typename KeyFinder<key, pairs_rest...>::type
+                >::type;
         };
 
     } // namespace detail
@@ -26,7 +62,6 @@ namespace typemap {
         static constexpr auto key = _key;
         using type = _type;
     };
-
 
     template<typename ... pairs>
     struct Map {
